1ded7b2bdddaec6bc583a813f7abfdece480dd6a
Jenimothy <33571610+Jenimothy@users.noreply.github.com>
2018-09-23 12:38:56 +0100

    Uploaded most recent version with SQL commeted out

--- wrldDatabaseFetch.php
+++ wrldDatabaseFetch.php
@@ -4,25 +4,42 @@
 
 //This code will be called by clicking on a desk, it will get the desk ID and send it over to here. This will find out the desks state and return it
 
-include 'ChineseRestaurantPart5.php';
+include 'dbconnect.php';
 
 $deskID = $_GET['q'];
 
-$finalData = 0001;
+$finalData = 0;
+/*
+//This section of code will contain the stuff to get from databse but issues currenty occur so commented out for now
+
 $db = connect();
 
 $sql = 'SELECT '.$deskID.' FROM 18indteam1db.staff;';
 
+$result = mysqli_query($database, $sql);
 
-//This section of code will contain the stuff to get from databse but that doesn't exist currently so it will be dummied instead
 
-$finalData = 1;
+while($row=mysqli_fetch_array($result))
+{
+	$finalData = $row['Occupied'];
+	
+}	
+
+//Fetches from database
+
+*/
+
+
+if (($deskID == 1) || ($deskID == 2) || ($deskID == 4)) //This is a placeholder for when the databse is sorted out
+{
+	$finalData = 1;
+}
 
 
 
 //This is the end of that code
 
 
-echo $deskID;
+echo $finalData;
 
 ?>
