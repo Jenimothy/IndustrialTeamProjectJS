30ef6e65ad128169a83f68886f6c102d0f534052
Aleksejs Loginovs (Student) <ALoginovs@dundee.ac.uk>
2018-09-26 10:41:21 +0100

    changed dominic's code to be more concise and make more sense

--- getHighlightedDesks.php
+++ getHighlightedDesks.php
@@ -1,35 +1,21 @@
-<?php
+<?php 
 
 include 'dbconnect.php';
 
 //This will return the status of all desks and if they are in use or not
 
 $db = connect();
-$finalId;
-$finalOc;
-
 $stmt = $db->prepare('SELECT desk_ID, Available FROM staff');
 $stmt->execute();
+
 $result = $stmt->get_result();
 
-$desks = new desk();
 $i = 0;
-while($row=mysqli_fetch_array($result))
-{
-	$finalOc[$i] = $row['Available'];
-	$finalId[$i] = $row['desk_ID'];
+while($row=$result->fetch_array(MYSQLI_ASSOC))
+{	
+	$desks_states[$i] = $row;
 	$i++;
 }
 
-$desks->id = $finalId;
-$desks->oc = $finalOc;
-
-echo json_encode($desks);
-
-class desk
-{
-	public $id;
-	public $oc;
-}
-
+echo json_encode($desks_states);
 ?>
